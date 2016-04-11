edcd33bc05b0a23fa34b1f976c4c621ac6f0f078
Harm de Vries <mail@harmdevries.com>
2016-04-11 15:53:04 -0400

    database wrapper + changes to the js

--- database/db_utils.py
+++ database/db_utils.py
@@ -1,28 +1,75 @@
 """Helper functions to connect to postgres database."""
 import psycopg2
+import psycopg2.extras
 import urlparse
 
 
-def db_connect(database, username, password, hostname, port):
-    """Connect to postgresql database."""
-    try:
-        conn = psycopg2.connect(database=database,
-                                user=username,
-                                password=password,
-                                host=hostname,
-                                port=port)
-        return conn
-    except Exception as e:
-        print "Unable to connect to database:"
-        print e.msg
+class DatabaseHelper():
+    """Database helper for multimodal dialogue project."""
+
+    def __init__(self, database, username, password, hostname, port):
+        """Connect to postgresql database."""
+        try:
+            self.conn = psycopg2.connect(database=database,
+                                         user=username,
+                                         password=password,
+                                         host=hostname,
+                                         port=port)
+        except Exception as e:
+            print "Unable to connect to database:"
+            print e.msg
+
+    @classmethod
+    def from_postgresurl(cls, db_url):
+        """Return DatabaseHelper from postgresurl.
+
+        Parses postgresurl into database, username, password, hostname
+        and port and initializes DatabaseHelper from it.
+        """
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(db_url)
+        return cls(url.path[1:], url.username, url.password,
+                   url.hostname, url.port)
+
+    def get_picture(self, id):
+        """Get image_url and its annotations by picture_id."""
+        try:
+            cur = self.conn.cursor()
+            cur.execute('SELECT coco_url FROM picture '
+                        'WHERE picture_id = %s', [id])
+            coco_url, = cur.fetchone()
+            cur.close()
+        except Exception as e:
+            print "Unable to get image url from picture id"
+            print e
+        try:
+            cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cur.execute(('SELECT '
+                         'o.object_id, o.category_id, c.name, o.segment, '
+                         'o.area FROM object AS o, object_category AS c '
+                         'WHERE o.category_id = c.category_id AND '
+                         'o.picture_id = %s '
+                         'ORDER BY o.area ASC'), [id])
+            rows = cur.fetchall()
+        except Exception as e:
+            print "Unable to get annotations"
+            print e
+
+        return coco_url, rows
+
+    def insert(self, picture_id, object_id):
+        """Insert new dialogue."""
+        # curr = self.conn.cursor()
+        # curr.execute("INSERT INTO dialogue (picture_id, ann_id) "
+        #              "VALUES(%s, %s)", (picture_id, object_id))
+        # curr.execute("SELECT currval(pg_get_serial_sequence("
+        #              "'dialogues','id'))")
+        # self.conn.commit()
+        raise NotImplementedError()
 
 
-def parse_postgresurl(db_url):
-    """Parse postgresql url.
-
-    Returns database, username, password,
-    hostname and port.
-    """
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(db_url)
-    return url.path[1:], url.username, url.password, url.hostname, url.port
+if __name__ == '__main__':
+    import os
+    db = DatabaseHelper.from_postgresurl(os.environ['DATABASE_URL'])
+    url, annotations = db.get_picture(9)
+    print annotations[0].keys()
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 import random
 import socketio
 from flask import Flask, render_template
-from database.db_utils import parse_postgresurl, db_connect
+from database.db_utils import DatabaseHelper
 # from flask.ext.login import LoginManager, UserMixin, login_required
 
 
@@ -27,11 +27,8 @@ clients_partner = {}  # Socketid of client's partner
 clients_did = {}  # Dialogue id of client
 dialogue_anns = {}  # All annotations for dialogue
 dialogue_annind = {}  # Selected index of image annotations
-
 """ Database connection """
-database, username, password, hostname, port = parse_postgresurl(
-    os.environ["DATABASE_URL"])
-conn = db_connect(database, username, password, hostname, port)
+db = DatabaseHelper.from_postgresurl(os.environ['DATABASE_URL'])
 
 
 @app.route('/')
@@ -71,7 +68,7 @@ def new_answer(sid, message):
 @sio.on('guess', namespace='/game')
 def guess(sid, obj):
     did = clients_did[sid]
-    objs = [x for x in dialogue_anns[did]]
+    objs = dialogue_anns[did]
     sio.emit('all annotations', {'partner': False, 'objs': objs},
              room=sid, namespace='/game')
     sio.emit('all annotations', {'partner': True},
@@ -82,7 +79,7 @@ def guess(sid, obj):
 def guess_annotation(sid, ann_id):
     did = clients_did[sid]
     ann_ind = dialogue_annind[did]
-    if dialogue_anns[did][ann_ind]['id'] == ann_id:
+    if dialogue_anns[did][ann_ind]['object_id'] == ann_id:
         sio.emit('correct annotation', {'partner': False},
                  room=sid, namespace='/game')
         sio.emit('correct annotation', {'partner': True},
@@ -95,6 +92,32 @@ def guess_annotation(sid, ann_id):
     logout([sid, clients_partner[sid]])
 
 
+def process_annotations(annotations):
+    """Preprocess annotations.
+
+    Convert area attribute from Decimal to float
+    because json encode can not handle Decimal.
+
+    Also convert segment coordinates from a single list into
+    x and y coordinate list.
+    """
+    anns = []
+    for ann in annotations:
+        ann = dict(ann)
+        ann['area'] = float(ann['area'])
+
+        segments = []
+        for seg in ann['segment']:
+            x = []
+            y = []
+            for i in range(0, len(seg), 2):
+                x.append(seg[i])
+                y.append(seg[i + 1])
+            segments.append({'x': x, 'y': y})
+        ann['segment'] = segments
+        anns.append(ann)
+    return anns
+
 @sio.on('next', namespace='/game')
 def find_partner(sid):
     partnerid = False
@@ -103,29 +126,18 @@ def find_partner(sid):
             partnerid = id
 
     if partnerid:
+        dialogue_id = 1
         clients_partner[partnerid] = sid
         clients_partner[sid] = partnerid
         role = (random.random() > 0.5)
-        ind = 9
-        cur = conn.cursor()
-        cur.execute('SELECT coco_url FROM picture WHERE'
-                    'picture_id = %s', ind)
-        coco_url, = cur.fetchone()
-        cur.close()
-        cur = conn.cursor()
-        ann_ind = random.randint(0, len(obj['annotations']) - 1)
-        ann = obj['annotations'][ann_ind]
+        coco_url, annotations = db.get_picture(9)
+        annotations = process_annotations(annotations)
+        ann_ind = random.randint(0, len(annotations) - 1)
+        ann = annotations[ann_ind]
 
-        # curr = conn.cursor()
-        # curr.execute("INSERT INTO dialogues(image_id, ann_id) VALUES(%s, %s)",
-        #              (obj['id'], ann['id']))
-        # curr.execute("SELECT currval(pg_get_serial_sequence("
-        #              "'dialogues','id'))")
-        dialogue_id, = curr.fetchone()
-        conn.commit()
         clients_did[id] = dialogue_id
         clients_did[sid] = dialogue_id
-        dialogue_anns[dialogue_id] = obj['annotations']
+        dialogue_anns[dialogue_id] = annotations
         dialogue_annind[dialogue_id] = ann_ind
 
         if role:
@@ -135,19 +147,13 @@ def find_partner(sid):
                      namespace='/game')
             sio.emit('answerer',
                      {'img': coco_url,
-                      'poly_x': ann['poly_x'],
-                      'poly_y': ann['poly_y'],
-                      'name': ann['category'],
-                      'catid': ann['catid']},
+                      'object': ann},
                      room=sid,
                      namespace='/game')
         else:
             sio.emit('answerer',
                      {'img': coco_url,
-                      'poly_x': ann['poly_x'],
-                      'poly_y': ann['poly_y'],
-                      'name': ann['category'],
-                      'catid': ann['catid']},
+                      'object': ann},
                      room=id,
                      namespace='/game')
             sio.emit('questioner',
