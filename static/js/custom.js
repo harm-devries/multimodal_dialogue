586899e588fe0e2f2f4d69f68fa947ea8cb7b7c8
Harm de Vries <mail@harmdevries.com>
2016-04-20 11:53:19 -0400

    https for socketio

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     namespace = '/game';
-    var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
+    var socket = io.connect('https://' + document.domain + ':' + location.port + namespace);
     var image_src; //image url
     var object; // selected object for oracle
     var correct_obj; // if flag is true, segment will be displayed in green
