60d0449e25024780edff9ee94dfa27512a5fbe95
Harm de Vries <mail@harmdevries.com>
2016-07-07 11:24:06 -0400

    instructions ++

---
+++ static/js/instructions.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    function updateItems(delta)
+    {
+        var $items = $('#group').children();
+        var $current = $items.filter('.current');
+        var index = $current.index();
+        var newIndex = index+delta;
+        // Range check the new index
+        console.log($items.length);
+        newIndex = (newIndex < 0) ? 0 : ((newIndex > $items.length) ? $items.length : newIndex); 
+        if (newIndex != index){
+            $current.removeClass('current');
+            $current = $items.eq(newIndex).addClass('current');
+            // Hide/show the next/prev
+            $("#prevbtn").toggle(!$current.is($items.first()));    
+            $("#nextbtn").toggle(!$current.is($items.last()));
+            $('#newgame').toggle($current.is($items.last()));    
+        }
+    }
+    $('#nextbtn').click(function(event) {
+        updateItems(1);
+    });
+    $('#prevbtn').click(function(event) {
+        updateItems(-1);
+    });
+});
