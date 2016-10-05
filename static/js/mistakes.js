47cd34458825743bbdde9de261e0beaf99eadadc
fstrub <florian.strub@gmail.com>
2016-10-05 15:23:38 +0200

    remove copy/past

--- static/js/mistakes.js
+++ static/js/mistakes.js
@@ -6,6 +6,7 @@
 
 $(document).ready(function() {
 
+
     //require to load theme
     vex.defaultOptions.className = 'vex-theme-default';
 
@@ -44,4 +45,32 @@ $(document).ready(function() {
         }
 
     });
+
+
+    //disable copy/past source : http://stackoverflow.com/questions/9958478/how-to-disable-copy-paste-browser
+
+    //disable mouse drag select start
+    document.onselectstart = new Function('return false');
+    function dMDown(e) { return false; }
+    //function dOClick() { return true; }
+    //document.onmousedown = dMDown;
+    //document.onclick = dOClick;
+
+    $("#document").attr("unselectable", "on");
+
+    //disable mouse drag select end  disable right click - context menu
+    document.oncontextmenu = new Function("return false");
+
+    //disable CTRL+A/CTRL+C through key board start
+    function disableSelectCopy(e) {
+    // current pressed key
+        var pressedKey = String.fromCharCode(e.keyCode).toLowerCase();
+        if (e.ctrlKey && (pressedKey == "c" || pressedKey == "x" || pressedKey == "v" || pressedKey == "a")) {
+            return false;
+        }
+
+    }
+
+    document.onkeydown = disableSelectCopy;
+
 });
